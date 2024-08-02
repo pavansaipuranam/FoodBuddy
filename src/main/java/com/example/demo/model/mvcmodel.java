b906a99857523aad2f0d2bc4b7c81a449da5ab17
Pavan Sai <pavansai4741424@gmail.com>
2024-08-02 12:35:24 +0530

    pavan

---
+++ src/main/java/com/example/demo/model/mvcmodel.java
@@ -0,0 +1,66 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class mvcmodel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+	
+	private String name;
+	
+	public mvcmodel(int id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public mvcmodel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	private String email;
+	
+	private String password;
+
+}
